0af16422371d57c8d27800582985c292c7f6e236
Young <17671203920@163.com>
2022-03-08 22:42:46 +0800

    fixed nav/footer were coding every page

--- src/main/webapp/js/page.js
+++ src/main/webapp/js/page.js
@@ -1,20 +1,8 @@
 $(function(){
-    // $.get("nav.html",function (data) {
-    //     $("navbar").html(data);
-    // });
+    $.get("nav.html",function (data) {
+        $("#navList").html(data);
+    });
     $.get("footer.html",function (data) {
         $(".footer").html(data);
     });
-    $(".userindex").on('click',function(){
-        $(location).attr('href','index.html');
-    })
-    $(".allgame").on('click',function(){
-        $(location).attr('href','allGames.html');
-    })
-    $(".myinfo").on('click',function(){
-        $(location).attr('href','personalInfo.html');
-    })
-    $(".aboutme").on('click',function(){
-        $(location).attr('href','index.html');
-    })
 })
